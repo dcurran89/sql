a10f9af8f8073b3a3de147edc74bd0d13345e2b4
Daniel Curran <danielcurran@Daniels-MacBook-Pro.local>
2016-05-09 10:35:54 -0500

    using order by on queries

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -1,15 +1,13 @@
 
 SELECT first_name, last_name, gender
 FROM employees
-WHERE gender = 'M' AND (first_name ='Irena' OR first_name = 'Vidya' OR first_name = 'Maya');
+WHERE gender = 'M' AND (first_name ='Irena' OR first_name = 'Vidya' OR first_name = 'Maya')
+ORDER BY first_name, last_name;
 
-SELECT first_name, last_name
+SELECT first_name, last_name, emp_no
 FROM employees
-WHERE last_name LIKE 'E%' OR last_name LIKE '%E';
-
-SELECT first_name, last_name
-FROM employees
-WHERE last_name LIKE 'E%' AND last_name LIKE '%E';
+WHERE last_name LIKE 'E%' AND last_name LIKE '%E'
+ORDER BY emp_no DESC;
 
 -- SELECT first_name, last_name, hire_date
 -- FROM employees
@@ -19,10 +17,11 @@ WHERE last_name LIKE 'E%' AND last_name LIKE '%E';
 -- FROM employees
 -- WHERE birth_date LIKE '%-12-25';
 
-SELECT first_name, last_name, hire_date
+SELECT first_name, last_name, hire_date, birth_date
 FROM employees
 WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31'
-    AND birth_date LIKE '%-12-25';
+    AND birth_date LIKE '%-12-25'
+ORDER BY birth_date, hire_date DESC;
 
 SELECT first_name, last_name
 FROM employees
