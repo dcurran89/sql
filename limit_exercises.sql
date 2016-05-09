65e626c14fe208365e6da09a65e9b532b402060b
Daniel Curran <danielcurran@Daniels-MacBook-Pro.local>
2016-05-09 11:43:50 -0500

    using limit on queries

---
+++ limit_exercises.sql
@@ -0,0 +1,20 @@
+-- Exercise 8.7.5 Limit
+
+SELECT first_name, last_name
+FROM employees
+GROUP BY last_name DESC
+LIMIT 10;
+
+SELECT first_name, last_name, hire_date, birth_date
+FROM employees
+WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31'
+    AND birth_date LIKE '%-12-25'
+ORDER BY birth_date, hire_date DESC
+LIMIT 5;
+
+SELECT first_name, last_name, hire_date, birth_date
+FROM employees
+WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31'
+    AND birth_date LIKE '%-12-25'
+ORDER BY birth_date, hire_date DESC
+LIMIT 5 OFFSET 45;
