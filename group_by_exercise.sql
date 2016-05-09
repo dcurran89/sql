27d9756961a688b58094a5c89e3bda58728aff9f
Daniel Curran <danielcurran@Daniels-MacBook-Pro.local>
2016-05-09 11:17:28 -0500

    using group by on queries

---
+++ group_by_exercise.sql
@@ -0,0 +1,19 @@
+SELECT title
+FROM titles
+GROUP BY title ASC;
+
+SELECT first_name, last_name, emp_no
+FROM employees
+WHERE last_name LIKE 'E%' AND last_name LIKE '%E'
+GROUP BY first_name ASC;
+
+SELECT first_name, last_name, emp_no
+FROM employees
+WHERE last_name LIKE 'E%' AND last_name LIKE '%E'
+GROUP BY first_name, last_name ASC;
+
+SELECT first_name, last_name
+FROM employees
+WHERE last_name LIKE '%q%' 
+ AND last_name NOT LIKE '%qu%'
+GROUP BY last_name;
