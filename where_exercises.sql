a3d7f7c6a2ca0f3e55ae769a3892f2782173dc47
Daniel Curran <danielcurran@Daniels-MacBook-Pro.local>
2016-05-09 09:49:20 -0500

    evaluation grouping using AND

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,19 +1,35 @@
-SELECT first_name, last_name
+SELECT first_name, last_name, gender
 FROM employees
-WHERE first_name IN ('Irena', 'Vidya', 'Maya');
+WHERE gender = 'M' AND (first_name ='Irena' OR first_name = 'Vidya' OR first_name = 'Maya');
 
 SELECT first_name, last_name
 FROM employees
-WHERE last_name LIKE 'E%';
+WHERE last_name LIKE 'E%' OR last_name LIKE '%E';
+
+SELECT first_name, last_name
+FROM employees
+WHERE last_name LIKE 'E%' AND last_name LIKE '%E';
+
+-- SELECT first_name, last_name, hire_date
+-- FROM employees
+-- WHERE hire_date BETWEEN '1990-01-01' AND '1999-01-01';
+
+-- SELECT first_name, last_name, birth_date
+-- FROM employees
+-- WHERE birth_date LIKE '%-12-25';
 
 SELECT first_name, last_name, hire_date
 FROM employees
-WHERE hire_date BETWEEN '1990-01-01' AND '1999-01-01';
-
-SELECT first_name, last_name, birth_date
-FROM employees
-WHERE birth_date LIKE '%-12-25';
+WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31'
+    AND birth_date LIKE '%-12-25';
 
 SELECT first_name, last_name
 FROM employees
-WHERE last_name LIKE '%q%';
+WHERE last_name LIKE '%q%' 
+ AND last_name NOT LIKE '%qu%';
+
+ -- safer version of above
+ SELECT first_name, last_name
+FROM employees
+WHERE last_name LIKE CONCAT('%', 'q' ,'%') 
+ AND last_name NOT LIKE CONCAT('%', 'qu' ,'%');
