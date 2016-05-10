3ef3cec8d3858609032eb4ca78529ba1fc9a0afd
Daniel Curran <danielcurran@Daniels-MacBook-Pro.local>
2016-05-10 13:54:18 -0500

    updated table name from albums_migration to albums

--- update_exercises.sql
+++ update_exercises.sql
@@ -1,13 +1,13 @@
 USE codeup_test_db;
 
-SELECT Name AS 'All Albums In Table', Sales FROM albums_migration;
-UPDATE albums_migration SET Sales = (Sales * 10);
-SELECT * FROM albums_migration;
+SELECT Name AS 'All Albums In Table', Sales FROM albums;
+UPDATE albums SET Sales = (Sales * 10);
+SELECT * FROM albums;
 
-SELECT Name AS 'Albums Released Before 1980' FROM albums_migration WHERE Release_Date < 1980;
-UPDATE albums_migration SET Release_Date = 1901 WHERE Release_Date < 1980;
-SELECT * FROM albums_migration;
+SELECT Name AS 'Albums Released Before 1980' FROM albums WHERE Release_Date < 1980;
+UPDATE albums SET Release_Date = 1901 WHERE Release_Date < 1980;
+SELECT * FROM albums;
 
-SELECT Name AS 'All Michael Jackson Albums' FROM albums_migration WHERE Artist = 'Michael Jackson';
-UPDATE albums_migration SET Artist = 'Peter Jackson' WHERE Artist = 'Michael Jackson';
-SELECT * FROM albums_migration;
+SELECT Name AS 'All Michael Jackson Albums' FROM albums WHERE Artist = 'Michael Jackson';
+UPDATE albums SET Artist = 'Peter Jackson' WHERE Artist = 'Michael Jackson';
+SELECT * FROM albums;
